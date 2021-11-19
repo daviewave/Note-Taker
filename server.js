804e8c3211e4de298c4af6fcc750bbc2480675ec
David Silveira <daviewave@gmail.com>
2021-11-18 19:58:53 -0500

    was getting an error when after setting up post function in apiRouter but figured out i needed to add my app.use's so that express would be able to handle data parsing

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const htmlRoute = require("./routes/htmlRouter");
 //set the port for the server
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
 require("./routes/htmlRouter")(app);
 require("./routes/apiRouter")(app);
 
