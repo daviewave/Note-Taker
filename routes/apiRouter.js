d9c4ecd4fe441531442f2a034e19beaed7a18f0f
David Silveira <daviewave@gmail.com>
2021-11-18 20:00:05 -0500

    added a post function in apiRouter.js working fully functional

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 module.exports = function (app) {
   // API GET Requests
@@ -11,7 +12,28 @@ module.exports = function (app) {
     response.json(data);
   });
 
-  app.post("/api/notes", function (request, response) {});
+  app.post("/api/notes", function (request, response) {
+    //store the info from the new note in a variable 'newNote'
+    const newNote = request.body;
+    console.log("NewNote: " + newNote);
+    //confirm a request is being made
+    console.log(
+      "POST REQUEST -- NEW NOTE: '" + JSON.stringify(newNote) + "' BEING ADDED."
+    );
+    //create unique id and assign it to the new note
+    newNote.id = uuidv4;
+
+    //turn the object into somehing we can send to the database with json.parse and specify the file sending to
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    //add new note to list of data from db.json
+    data.push(newNote);
+    //write the new data list of objects from data to the database
+    fs.writeFileSync("./db/db.json", JSON.stringify(data));
+    //confirm that the file write was a success
+    console.log("SUCCESS: NEW NOTE POSTED(ADDED) TO THE DATABASE");
+
+    response.json(data);
+  });
 
   app.delete("/api/notes", function (request, response) {});
 };
